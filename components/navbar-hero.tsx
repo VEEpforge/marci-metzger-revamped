6ea4aa0f17b8284ecf424aa6b3d7aac13670ac78
Ezra Earl Villanueva <ezraearlvillanueva@gmail.com>
2025-12-08 10:38:19 +0800

    Remove unused files

--- components/navbar-hero.tsx
+++
@@ -1,8 +0,0 @@
-
-const NavbarHero = () => {
-  return (
-    <div className='fixed top-0 w-full bg-transparent z-50'>NavbarHero</div>
-  )
-}
-
-export default NavbarHero
--- components/navbar-secondary.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NavbarSecondary = () => {
-  return (
-    <div className='fixed top-0 w-full bg-transparent z-50'>NavbarSecondary</div>
-  )
-}
-
-export default NavbarSecondary
